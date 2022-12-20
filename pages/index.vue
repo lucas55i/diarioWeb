8ded12082e89ffd267fa14d04df51118b98970f3
Lucas Silva <lucas_55i@outlook.com>
2022-12-20 15:15:28 -0300

    Inicio do projeto

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <example />
+</template>
+
+<script lang="ts">
+import example from "~/components/exemple.vue";
+export default {
+  components: {
+    example,
+  },
+};
+</script>
+
+<style>
+</style>
