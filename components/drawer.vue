1203f3ef2a0cf505b99d9e689f60f6c5f10a05a4
Lucas S C. Jesus <lucas.devj@outlook.com>
2023-01-22 18:58:53 -0300

    Inclusão do c-stack para o conteúdo dos posts

--- components/drawer.vue
+++ components/drawer.vue
@@ -1,55 +1,74 @@
 <template>
- <div class="contianer">
-    <c-button ref="btnRef" @click="isOpen =true">Criar Post</c-button>
-
-    <c-drawer :isOpen="isOpen" placement="left" :on-close="close" :initialFocusRef="()=>$refs.inputInsideModal">
+  <div class="contianer">
+    <c-button ref="btnRef" @click="isOpen = true">Criar Post</c-button>
+    <c-drawer
+      :isOpen="isOpen"
+      placement="left"
+      :on-close="close"
+      :initialFocusRef="() => $refs.inputInsideModal"
+    >
       <c-drawer-overlay />
       <c-drawer-content>
-      <c-drawer-close-button />
-      <c-drawer-header>Crie Seu Post</c-drawer-header>
+        <c-drawer-close-button />
+        <c-drawer-header>Crie Seu Post</c-drawer-header>
 
-      <c-drawer-body>
-          <addUser/>
-      </c-drawer-body>
+        <c-drawer-body>
+          <addUser />
+        </c-drawer-body>
 
-      <c-drawer-footer>
-        <c-button variant="outline" mr="3" @click="isOpen = false">Cancelar</c-button>
-        <c-button variant-color="blue">Publicar Post</c-button>
-      </c-drawer-footer>
+        <c-drawer-footer>
+          <c-button variant="outline" mr="3" @click="isOpen = false"
+            >Cancelar</c-button
+          >
+          <c-button variant-color="blue">Publicar Post</c-button>
+        </c-drawer-footer>
       </c-drawer-content>
     </c-drawer>
-    <CDivider/>
+    <CDivider />
 
+    <c-stack :spacing="5"  v-for="user in posts">
+      <c-box :p="5" shadow="md" border-width="1px">
+        <c-heading>{{user.author}}</c-heading>
+        <c-text :mt="4">{{user.conteudo}}</c-text>
+      </c-box>
+    </c-stack>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import addUser from "~/components/add_post.vue"
+import addUser from "~/components/add_post.vue";
 
 export default Vue.extend({
-  name: 'IndexPage',
+  name: "IndexPage",
   components: {
-    addUser
+    addUser,
   },
-  inject: ['$chakraColorMode', '$toggleColorMode'],
   data() {
     return {
-      isOpen: false
-    }
+      isOpen: false,
+    };
   },
   computed: {
-   
+    posts(): any {
+      return this.$store.state.text.posts;
+    },
   },
   methods: {
-    close () {
-      this.isOpen = false
-    }
-  }
-})
+    close() {
+      this.isOpen = false;
+    },
+    getPosts() {
+      this.$store.dispatch("text/consultarPosts");
+    },
+  },
+  created() {
+    this.getPosts();
+  },
+});
 </script>
 <style>
-.container{
+.container {
   padding-top: 20px;
   padding-left: 20px;
 }
