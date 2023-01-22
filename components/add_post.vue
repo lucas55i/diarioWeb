fafc197c7a2f718ed9e3d71fbbd11a3fa1e6607a
Lucas S C. Jesus <lucas.devj@outlook.com>
2023-01-22 20:00:36 -0300

    Implementação do componente de criar post

--- components/add_post.vue
+++ components/add_post.vue
@@ -1,27 +1,30 @@
 <template>
- <div class="contianer">
- <c-form-control>
-          <c-stack spacing="24px">
-              <c-box>
-                <c-form-label for="username">Nome</c-form-label>
-                <c-input
-                v-model="post.author"
-                  ref="firstField"
-                  id="username"
-                  placeholder="Digite seu nome"
-                />
-              </c-box>
+  <div class="contianer">
+    <c-form-control ref="criarPostForm">
+      <c-stack spacing="24px">
+        <c-box>
+          <c-form-label for="username">Nome</c-form-label>
+          <c-input
+            v-model="post.author"
+            id="username"
+            placeholder="Digite seu nome"
+          />
+        </c-box>
 
-              <c-box>
-                <c-form-label for="desc">Conteúdo do Post</c-form-label>
-                <c-textarea id="desc" v-model="post.conteudo"/>
-              </c-box>
-          </c-stack>
-          <c-drawer-footer>
-        <c-button variant="outline" mr="3" @click="isOpen = false">Cancelar</c-button>
-        <c-button variant-color="blue">Publicar Post</c-button>
+        <c-box>
+          <c-form-label for="desc">Conteúdo do Post</c-form-label>
+          <c-textarea id="desc" v-model="post.conteudo" />
+        </c-box>
+      </c-stack>
+      <c-drawer-footer>
+        <c-button variant="outline" mr="3" @click="close()"
+          >Cancelar</c-button
+        >
+        <c-button variant-color="blue" @click="criarPost()"
+          >Publicar Post</c-button
+        >
       </c-drawer-footer>
-        </c-form-control>
+    </c-form-control>
   </div>
 </template>
 
@@ -30,26 +33,30 @@ import Vue from "vue";
 import { AddPost } from "~/core/models/post.model";
 
 export default Vue.extend({
-  components: {
-
-  },
+  components: {},
   data() {
     return {
       isOpen: false,
       post: {
         author: "",
-        conteudo: ""
-      } as AddPost
-    }
+        conteudo: "",
+      } as AddPost,
+    };
   },
-  computed: {
-   
-  },
+  computed: {},
   methods: {
-    
-  }
-})
+    async criarPost() {
+      const addPost = { ...this.post };
+      await this.$store.dispatch("text/criarPost", addPost);
+      this.$emit('postCriado')
+      this.$emit('closeDrawer')
+    },
+
+    close() {
+      this.isOpen = true;
+    },
+  },
+});
 </script>
 <style>
-
 </style>
