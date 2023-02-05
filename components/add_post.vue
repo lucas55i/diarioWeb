8a49eb64a09d11c82024efacc4cd79f0dac57ed6
Lucas S C. Jesus <lucas.devj@outlook.com>
2023-02-04 22:35:16 -0300

    Verificação para que todos os campos sejam preenchidos ao cadastro.

--- components/add_post.vue
+++ components/add_post.vue
@@ -1,28 +1,20 @@
 <template>
   <div class="contianer">
-    <c-form-control ref="criarPostForm">
+    <c-form-control ref="criarPostForm" is-required>
       <c-stack spacing="24px">
         <c-box>
           <c-form-label for="username">Nome</c-form-label>
-          <c-input
-            v-model="post.author"
-            id="username"
-            placeholder="Digite seu nome"
-          />
+          <c-input type="text" v-model="post.author" id="username" placeholder="Digite seu nome" />
         </c-box>
 
         <c-box>
           <c-form-label for="desc">Conteúdo do Post</c-form-label>
-          <c-textarea id="desc" v-model="post.conteudo" />
+          <c-textarea aria-required="true" id="desc" v-model="post.conteudo" />
         </c-box>
       </c-stack>
       <c-drawer-footer>
-        <c-button variant="outline" mr="3" @click="close()"
-          >Cancelar</c-button
-        >
-        <c-button variant-color="blue" @click="criarPost()"
-          >Publicar Post</c-button
-        >
+        <c-button variant="outline" mr="3" @click="close()">Cancelar</c-button>
+        <c-button variant-color="blue" @click="criarPost()">Publicar Post</c-button>
       </c-drawer-footer>
     </c-form-control>
   </div>
@@ -37,19 +29,24 @@ export default Vue.extend({
   data() {
     return {
       isOpen: false,
-      post: {
-        author: "",
-        conteudo: "",
-      } as AddPost,
+      post: {} as AddPost,
     };
   },
   computed: {},
   methods: {
     async criarPost() {
       const addPost = { ...this.post };
-      await this.$store.dispatch("text/criarPost", addPost);
-      this.$emit('postCriado')
-      
+      if (!this.post.author || !this.post.conteudo) {
+        this.$toast({
+          title: 'Preencha todos os campos',
+          status: 'warning',
+          duration: 2000
+        })
+      } else {
+        await this.$store.dispatch("text/criarPost", addPost);
+        this.$emit('postCriado')
+      }
+
     },
 
     close() {
