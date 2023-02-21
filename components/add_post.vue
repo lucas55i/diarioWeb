cee5a6ea31280cc003f78e446067aa699d6fe9b4
Lucas S C. Jesus <lucas.devj@outlook.com>
2023-02-21 00:38:48 -0300

    Implementando css (form add post)

--- components/add_post.vue
+++ components/add_post.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="contianer">
+  <div class="add-post-container">
     <c-form-control ref="criarPostForm" is-required>
       <c-stack spacing="24px">
         <c-box>
-          <c-form-label for="username">Nome</c-form-label>
+          <c-form-label class="label" for="username">Nome</c-form-label>
           <c-input type="text" v-model="post.author" id="author" placeholder="Digite seu nome" />
         </c-box>
 
         <c-box>
-          <c-form-label for="desc">Conteúdo do Post</c-form-label>
+          <c-form-label class="label" for="desc">Conteúdo do Post</c-form-label>
           <c-textarea aria-required="true" id="conteudo" v-model="post.conteudo" />
         </c-box>
       </c-stack>
       <c-drawer-footer>
-        <c-button variant="outline"  @click="close()">Cancelar</c-button>
+        <c-button variant="outline" @click="close()">Cancelar</c-button>
         <c-button variant-color="blue" @click="criarPost()">Publicar Post</c-button>
       </c-drawer-footer>
     </c-form-control>
@@ -57,4 +57,22 @@ export default Vue.extend({
 });
 </script>
 <style>
+.label {
+  color: #eee;
+}
+
+.add-post-container {
+  margin: 15px 0;
+  width: 100%;
+}
+
+.add-post-input {
+  height: 40px;
+  padding: 0 10px;
+  border-radius: 5px;
+  border: none;
+  flex: 2;
+  background-color: #444;
+  font-size: 16px;
+}
 </style>
